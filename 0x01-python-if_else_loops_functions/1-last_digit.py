c6fcc69a50577a6ebdfdd552079fac9eda79a0ba
Ben Keener <keener4christ@gmail.com>
2019-12-10 22:17:59 +0000

    Fixed indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 lastdig = abs(number) % 10 * (-1 if number < 0 else 1)
 if (lastdig > 5):
     print("Last digit of {} is {} and is greater than 5".format(number,
-                                                                    lastdig))
+                                                                lastdig))
 elif (lastdig == 0):
     print("Last digit of {} is {} and is 0".format(number, lastdig))
 else:
