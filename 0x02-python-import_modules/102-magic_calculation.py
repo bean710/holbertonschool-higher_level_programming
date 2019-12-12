d9368cdf453fa777d63288d96ee07db64771a006
Ben Keener <keener4christ@gmail.com>
2019-12-12 22:05:22 +0000

    Removed 'dis'

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -13,5 +11,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-
-dis.dis(magic_calculation)
