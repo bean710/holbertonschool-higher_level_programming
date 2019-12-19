597489a99eac78922317c010d857634509efec7f
Ben Keener <keener4christ@gmail.com>
2019-12-19 22:58:00 +0000

    Simplified loop

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(d):
-    for k, v in {key: d[key] for key in sorted(d.keys())}.items():
+    for k, v in d.iteritems():
         print("{}: {}".format(k, v))
