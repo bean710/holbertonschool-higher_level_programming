ab1cd69b00d9b895906d55918f36af111ef0b36d
Ben Keener <keener4christ@gmail.com>
2019-12-19 22:01:26 +0000

    Fixed formatting

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     new_list = [(x if not x == search else replace) for x in my_list]
-    #new_list = [my_list[my_list.index(search)] = replace for x in
-    #            my_list.count(search)]
+#    new_list = [my_list[my_list.index(search)] = replace for x in
+#                my_list.count(search)]
     return new_list
