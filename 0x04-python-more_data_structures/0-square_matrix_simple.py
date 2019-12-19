fd1075820392c313e466cb19a966a1d132f131e2
Ben Keener <keener4christ@gmail.com>
2019-12-19 21:49:16 +0000

    Cleaned up

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_mat = list(map(list, matrix))
-    for i in range(len(new_mat)):
-        for j in range(len(new_mat[i])):
-            new_mat[i][j] = matrix[i][j] * matrix[i][j]
+    new_mat = [[x ** 2 for x in i] for i in new_mat]
 
     return new_mat
