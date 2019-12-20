927f6f67ffa45f62ba4ac353ca9c54e4ece12dd7
Ben Keener <keener4christ@gmail.com>
2019-12-20 03:08:17 +0000

    Changed edge case return

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return None
+    if (roman_string == None) or (not isinstance(roman_string, str)):
+        return 0
 
     nums = []
     for i in roman_string:
