0356c9fb21d1fbd84ae2144e2597d918d8fc6a8e
Ben Keener <keener4christ@gmail.com>
2019-12-20 00:00:11 +0000

    Completed task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    nmat = []
-    for row in matrix:
-        nmat.append(list(map(lambda x: x ** 2, row)))
-
-    return nmat
+    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
