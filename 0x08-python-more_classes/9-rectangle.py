de3d196b1b81f810cb3967db2183e9b82e588519
Ben Keener <keener4christ@gmail.com>
2020-01-15 00:23:21 +0000

    Fixed function definition

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,7 +18,8 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
-    @classmethod(cls, size):
+    @classmethod
+    def square(cls, size):
         """Returns a new rect as a square of size size"""
         return cls(size, size)
 
