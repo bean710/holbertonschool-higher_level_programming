53dc50e8fa9c2c6bccbaad60567162ae76546b88
Ben Keener <keener4christ@gmail.com>
2020-01-20 20:42:49 +0000

    Wrote list class with ordered print

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+"""This module contains a class that inherits `list` and adds a function to
+print the list sorted in ascending order"""
+
+
+class MyList(list):
+    """Special list class with a sorted-print function"""
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order"""
+
+        print(sorted(self))
