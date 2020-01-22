76cacbecec6d69e972a21f7566aac5a4ed1e67c1
Ben Keener <keener4christ@gmail.com>
2020-01-22 02:28:42 +0000

    Wrote function to write text to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function which writes given text to a given file"""
+
+
+def write_file(filename="", text=""):
+    """Writes `text` to `filename`
+
+    Args:
+        filename (str): The name of the file to write to
+        text (str): The text to write to the file
+    """
+    with open(filename) as f:
+        f.write(text)
