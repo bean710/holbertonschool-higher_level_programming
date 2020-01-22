427038592a3cdc2b6a642e3a997629b0722e7d47
Ben Keener <keener4christ@gmail.com>
2020-01-22 06:23:57 +0000

    Main guarded

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,27 +3,28 @@
 import sys
 
 
-lnum = 0
-codes = {}
-fsize = 0
-try:
-    for line in sys.stdin:
-        sep = line.split(" ")
+if __name__ == "__main__":
+    lnum = 0
+    codes = {}
+    fsize = 0
+    try:
+        for line in sys.stdin:
+            sep = line.split(" ")
 
-        fsize += int(sep[-1])
+            fsize += int(sep[-1])
 
-        if sep[-2] not in codes:
-            codes[sep[-2]] = 1
-        else:
-            codes[sep[-2]] += 1
+            if sep[-2] not in codes:
+                codes[sep[-2]] = 1
+            else:
+                codes[sep[-2]] += 1
 
-        lnum += 1
-        if line is None or lnum % 10 == 0:
-            print("File size: {}".format(fsize))
-            for k in sorted(codes):
-                print("{}: {}".format(k, codes[k]))
+            lnum += 1
+            if line is None or lnum % 10 == 0:
+                print("File size: {}".format(fsize))
+                for k in sorted(codes):
+                    print("{}: {}".format(k, codes[k]))
 
-except (KeyboardInterrupt, SystemExit):
-    print("File size: {}".format(fsize))
-    for k in sorted(codes):
-        print("{}: {}".format(k, codes[k]))
+    except (KeyboardInterrupt, SystemExit):
+        print("File size: {}".format(fsize))
+        for k in sorted(codes):
+            print("{}: {}".format(k, codes[k]))
