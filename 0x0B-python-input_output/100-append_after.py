c057acd2e61f2ff49dd4c9193a72696ec14c252a
Ben Keener <keener4christ@gmail.com>
2020-01-22 05:47:18 +0000

    Fixed not appending last line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,8 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, "r+") as f:
         contents = f.readlines()
-        for line in range(len(contents)):
-            if search_string in contents[line]:
-                contents.insert(line + 1, new_string)
+        new = []
+        olen = len(contents)
+        for line in contents:
+            new.append(line)
+            if search_string in line:
+                new.append(new_string)
         f.seek(0)
-        f.writelines(contents)
+        f.writelines(new)
