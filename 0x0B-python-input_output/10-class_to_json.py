8150b6a3d0c319fd2e19c4447e6267ee4d64034e
Ben Keener <keener4christ@gmail.com>
2020-01-22 04:18:39 +0000

    Wrote function to get class dict data

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a function which returns a class' dictionary as a JSON
+serializable object"""
+import json
+
+
+def class_to_json(obj):
+    """Converts `obj`s dictionary to a JSON serializable object
+
+    Args:
+        obj (class): The python class which dictionary is to be converted to a
+            JSON object
+
+    Returns:
+        str: `obj`s dictionary as a JSON serializable object
+    """
+    return obj.__dict__
