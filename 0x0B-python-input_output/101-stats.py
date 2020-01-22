80a73ce74f31179480fdb846b750fcec3d5375de
Ben Keener <keener4christ@gmail.com>
2020-01-22 06:18:24 +0000

    Wrote script to interperet data

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This script reads and reports on data from stdin"""
+import sys
+
+
+lnum = 0
+codes = {}
+fsize = 0
+try:
+    for line in sys.stdin:
+        sep = line.split(" ")
+
+        fsize += int(sep[-1])
+
+        if sep[-2] not in codes:
+            codes[sep[-2]] = 1
+        else:
+            codes[sep[-2]] += 1
+
+        lnum += 1
+        if line is None or lnum % 10 == 0:
+            print("File size: {}".format(fsize))
+            for k in sorted(codes):
+                print("{}: {}".format(k, codes[k]))
+
+except (KeyboardInterrupt, SystemExit):
+    print("File size: {}".format(fsize))
+    for k in sorted(codes):
+        print("{}: {}".format(k, codes[k]))
