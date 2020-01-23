df2cd352173477effb49f16c5432f79256cc9b4d
Ben Keener <keener4christ@gmail.com>
2020-01-23 20:47:24 +0000

    Created base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+"""This module contains a base class for all shape objects"""
+
+
+class Base():
+    """This is a base class for all shape objects which keeps track of IDs"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes an instance of Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+"""Module for testing the Base class"""
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+    """Tests the Base class"""
+
+    def setUp(self):
+        """Resets the base class to prevent interference"""
+        Base._Base__nb_objects = 0
+
+    def test_init(self):
+        """Tests proper initialization"""
+        a = Base()
+        self.assertEqual(type(a), Base)
+        b = Base(47)
+        self.assertEqual(type(b), Base)
+
+    def test_id(self):
+        """Tests that the correct IDs are being assigned"""
+        a = Base()
+        self.assertEqual(a.id, 1)
+        b = Base(47)
+        self.assertEqual(b.id, 47)
+        c = Base()
+        self.assertEqual(c.id, 2)
