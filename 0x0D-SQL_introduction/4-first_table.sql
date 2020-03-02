f7040a65c1f5ddb0c95d0154add04e6cb13c8079
Ben Keener <keener4christ@gmail.com>
2020-03-02 19:31:04 +0000

    Updated table specs

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
 -- This script creates a table
 -- Creates `first_table` if it doesn't already exist
-CREATE TABLE IF NOT EXISTS first_table;
+CREATE TABLE IF NOT EXISTS first_table(
+id INT,
+name VARCHAR(256)
+);
