ab9783dba6d0394ea67229225949d7b0d77bcbdd
Ben Keener <keener4christ@gmail.com>
2020-03-03 21:45:58 +0000

    Added quotes around names

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- This script lists the privileges for two users
 -- Prints the privileges for user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR "user_0d_1"@"localhost";
 -- Prints the privileges for user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR "user_0d_2"@"localhost";
