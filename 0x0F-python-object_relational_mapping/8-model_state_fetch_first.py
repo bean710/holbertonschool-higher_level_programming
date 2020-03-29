4e20bffd846157b4077eefad3d6060e93a5a83c8
Ben Keener <keener4christ@gmail.com>
2020-03-29 17:14:55 +0000

    Added case for no rows

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,4 +17,5 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
