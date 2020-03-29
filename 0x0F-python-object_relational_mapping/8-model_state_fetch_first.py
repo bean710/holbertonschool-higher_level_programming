aa51e0bb1411363c15875d6fe4789dcd6bb46217
Ben Keener <keener4christ@gmail.com>
2020-03-29 17:14:14 +0000

    Completed task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This script will fetch the first state in the table """
+
+if __name__ == "__main__":
+    from model_state import State, Base
+    from sqlalchemy import (create_engine, Table, Integer,
+                            String, Column)
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
