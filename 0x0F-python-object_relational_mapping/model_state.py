61daaaaa8c80aa1b41df92f21b519358bba00998
Ben Keener <keener4christ@gmail.com>
2020-03-29 16:34:11 +0000

    Added class docs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script creates a State class with SQLAlchemy
+"""This script creates a State class with SQLAlchemy"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
+    """ This class is a State class to interact with the ORM"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
