95c00a52d916575f445222061b473a49f0248924
Ben Keener <keener4christ@gmail.com>
2020-03-29 04:04:12 +0000

    Changed to query instead of full loop

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name=\"{}\" \
+                ORDER BY id".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
-        if (state[1] == sys.argv[4]):
-            print((state[0], state[1]))
+        print((state[0], state[1]))
