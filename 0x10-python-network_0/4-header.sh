270bd0a459e3f515b65f8edfa5170653ca4cebf2
Ben Keener <keener4christ@gmail.com>
2020-04-07 21:34:28 +0000

    added silent flag

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a header attribute with the request
-curl -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request with form data
-curl "$1" --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s "$1" --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
