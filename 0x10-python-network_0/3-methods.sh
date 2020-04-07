bbf68bee8ec59ab2785a52b0d41d8db60ce51cd1
Ben Keener <keener4christ@gmail.com>
2020-04-07 16:55:21 +0000

    Fixed to take an arg

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This script gets the accepted methods from an HTTP server
 
-curl -si -X OPTIONS 0:5000 | grep 'Allow:' | cut -f 1 -d " " --complement
+curl -si -X OPTIONS "$1" | grep 'Allow:' | cut -f 1 -d " " --complement
