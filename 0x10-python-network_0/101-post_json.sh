c5f77ef72824f805fbb0d1f3621332430e362ec6
Ben Keener <keener4christ@gmail.com>
2020-04-07 21:38:00 +0000

    Minor fixes

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script POSTs a json file
-
 curl -X POST -H "Content-Type: application/json" -d @$2 "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script gets a response from a super secret page
-
 curl -Ls -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0:5000/catch_me
