6bd979b337140aca05d7da3c95cead18b56363d9
Ben Keener <keener4christ@gmail.com>
2020-04-13 21:32:58 +0000

    Pep8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
 
-    res = requests.post("http://0.0.0.0:5000/search_user", data={"q":letter})
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
 
     try:
         jres = res.json()
