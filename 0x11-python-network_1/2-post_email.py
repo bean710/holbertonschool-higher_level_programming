7e4bda91acb5a57838b3e6c4688c8cb7628d111c
Ben Keener <keener4christ@gmail.com>
2020-04-13 19:12:51 +0000

    Completed task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """This module sends POST with an email"""
 import urllib.parse as parse
-import urllib.request as req
+import urllib.request as request
+import sys
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
+    data = parse.urlencode({"email": email}).encode("ascii")
+    req = request.Request(url, data)
+    with request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
