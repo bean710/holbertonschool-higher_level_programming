3be94d34330cf25c1ebb9c0559d950e777fa42db
Ben Keener <keener4christ@gmail.com>
2020-04-13 19:33:12 +0000

    Completed task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module handles urllib error codes"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
