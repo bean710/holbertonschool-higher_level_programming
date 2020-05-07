360638e9ba2c784c0ef69b184d1d405f6b13e8f0
Ben Keener <keener4christ@gmail.com>
2020-05-07 15:38:51 +0000

    Added edge cases

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,17 @@ def number_of_subscribers(subreddit):
     """Gets the number of subs for a subreddit"""
 
     res = requests.post("http://reddit.com/r/{}/about.json".format(subreddit),
-                        headers={"User-Agent": "python:com.benkeener:0.0.1"})
+                        headers={"User-Agent": "python:com.benkeener:0.0.1"},
+                        allow_redirects=False)
 
-    dat = res.json()
+    try:
+        dat = res.json()
 
-    subs = dat["data"]["subscribers"]
+        if "data" in dat and "subscribers" in dat["data"]:
+            subs = dat["data"]["subscribers"]
+        else:
+            subs = 0
+    except ValueError:
+        subs = 0
 
     return (subs)
