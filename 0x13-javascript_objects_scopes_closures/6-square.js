84ef6741bd4ca95a4147deaf0a8690e290ecedee
Ben Keener <keener4christ@gmail.com>
2020-05-11 20:57:37 +0000

    Inherits from old square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareOG = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareOG {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
