b3aa22f5414e1b47d12b0300c786d69b1f9efc82
Ben Keener <keener4christ@gmail.com>
2020-05-11 20:40:45 +0000

    Completed task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let y = 0; y < this.height; y++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const t = this.width;
+    this.width = this.height;
+    this.height = t;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
